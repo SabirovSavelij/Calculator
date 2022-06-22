24ac95fd58c9abdd052adccea16446d24d01ef52
SabirovSavelij <chisto.dla.githuba@gmail.com>
2022-06-22 09:37:05 +0300

    Second commit

--- XamlApp/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ XamlApp/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -188,7 +188,7 @@ namespace XamlApp {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/XamlApp;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/XamlApp;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
